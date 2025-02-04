ac2cdaca86fbae366d23ed913806be150459bf0a
Tien Thanh <nguyenthanh2542001@gmail.com>
2025-02-04 14:14:13 +0700

    Layout Onboarding_1 Screen

---
+++ app/src/main/java/com/example/androidapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.androidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_DELAY = 3000; // Thời gian delay 3 giây
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, OnboardingActivity.class);
+            startActivity(intent);
+            finish(); // Đóng SplashActivity để không quay lại được
+        }, SPLASH_DELAY);
+    }
+}
